7c2ae112f0aa66268cc05ba6c9bf9d7da1c249fe
Jordan Enev <jordan@devlabs.bg>
2020-08-30 17:33:55 +0300

    Change: Enable All CORS Requests.

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const  { models, connectDb } = require('./db/index')
 const app = express()
 const port = process.env.APP_PORT
+
+// Enable All CORS Requests
+app.use(cors())
 
 app.use((req, res, next) => {
   req.context = { models }
